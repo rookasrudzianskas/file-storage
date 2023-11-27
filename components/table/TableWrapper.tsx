7d9ed8b0c2779211b5cbe826b7cc70f41e7d08bb
Rokas <rookas.rudzenskas@gmail.com>
2023-11-27 16:36:14 +0200

    skeleton

--- components/table/TableWrapper.tsx
+++ components/table/TableWrapper.tsx
@@ -9,6 +9,7 @@ import {useUser} from "@clerk/nextjs";
 import {useCollection} from "react-firebase-hooks/firestore";
 import {collection, orderBy, query} from "@firebase/firestore";
 import {db} from "@/firebase";
+import { Skeleton } from "@/components/ui/skeleton"
 
 const TableWrapper = ({ skeletonFiles }: { skeletonFiles: FileType[] }) => {
   const { isLoaded, isSignedIn, user } = useUser();
@@ -36,12 +37,42 @@ const TableWrapper = ({ skeletonFiles }: { skeletonFiles: FileType[] }) => {
     }));
 
     setInitialFiles(files);
-  }, [docs])
+  }, [docs]);
+
+  if(docs?.docs.length === undefined) return (
+    <div className="flex flex-col">
+      <Button variant={"outline"} className="ml-auto w-36 h-10 mb-5">
+        <Skeleton className="h-5 w-full" />
+      </Button>
+
+      <div className="border rounded-lg">
+        <div className="border-b h-12" />
+        {skeletonFiles.map((file) => (
+          <div
+            key={file.id}
+            className="flex items-center space-x-4 p-5 w-full"
+          >
+            <Skeleton className="h-12 w-12" />
+            <Skeleton className="h-12 w-full" />
+          </div>
+        ))}
+
+        {skeletonFiles.length === 0 && (
+          <div className="flex items-center space-x-4 p-5 w-full">
+            <Skeleton className="h-12 w-12" />
+            <Skeleton className="h-12 w-full" />
+          </div>
+        )}
+      </div>
+    </div>
+  )
 
   return (
-    <div>
+    <div className="flex flex-col space-y-5 pb-10">
       <Button
         onClick={() => setSort(sort === "asc" ? "desc" : "asc")}
+        className="ml-auto w-fit"
+        variant={"outline"}
       >
         Sort By {sort === "desc" ? "Newest" : "Oldest"}
       </Button>
