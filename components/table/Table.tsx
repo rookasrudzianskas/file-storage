f42b0cb52e1b07ec7fe9968768db07b73e1bb307
Rokas <rookas.rudzenskas@gmail.com>
2023-11-27 17:19:34 +0200

    added a zustand

--- components/table/Table.tsx
+++ components/table/Table.tsx
@@ -17,7 +17,7 @@ import {
 } from "@/components/ui/table"
 import {FileType} from "@/typings";
 import {Button} from "@/components/ui/button";
-import {TrashIcon} from "lucide-react";
+import {PencilIcon, TrashIcon} from "lucide-react";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
@@ -33,6 +33,19 @@ export function DataTable<TData, TValue>({
     columns,
     getCoreRowModel: getCoreRowModel(),
   })
+
+
+
+  const openDeleteModal = (fileId: string) => {
+    setFileId(fileId);
+    setIsDeleteModalOpen(true);
+  }
+
+  const openRenameModal = (fileId: string, filename: string) => {
+    setFileId(fileId);
+    setFilename(filename);
+    setIsRenameModalOpen(true);
+  }
 
   return (
     <div className="rounded-md border">
@@ -64,7 +77,30 @@ export function DataTable<TData, TValue>({
               >
                 {row.getVisibleCells().map((cell) => (
                   <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    {cell.column.id === 'timestamp' ? (
+                      <div className="flex flex-col">
+                        <div className="text-sm">
+                          {(cell.getValue() as Date).toLocaleDateString()}
+                        </div>
+
+                        <div className="text-xs text-gray-500">
+                          {(cell.getValue() as Date).toLocaleTimeString()}
+                        </div>
+                      </div>
+                    ) : cell.column.id === 'filename' ? (
+                      <p
+                        // onClick={() => openRenameModal(
+                        //   (row.original as FileType).id,
+                        //   (row.original as FileType).filename,
+                        // )}
+                        className="underline flex items-center text-blue-500 hover:cursor-pointer"
+                      >
+                        {cell.getValue() as string}
+                        <PencilIcon size={15} className="ml-2" />
+                      </p>
+                    ) : (
+                      flexRender(cell.column.columnDef.cell, cell.getContext())
+                    )}
                   </TableCell>
                 ))}
 
---
+++ store/store.ts
@@ -0,0 +1,29 @@
+import { create } from 'zustand'
+
+interface AppState {
+  isDeleteModalOpen: boolean;
+  setIsDeleteModalOpen: (open: boolean) => void;
+
+  isRenameModalOpen: boolean;
+  setIsRenameModalOpen: (open: boolean) => void;
+
+  fileId: string | null;
+  setFileId: (fileId: string) => void;
+
+  filename: string | null;
+  setFilename: (filename: string) => void;
+}
+
+export const useAppStore = create<AppState>((set) => ({
+  fileId: null,
+  setFileId: (fileId: string) => set({ fileId }),
+
+  filename: null,
+  setFilename: (filename: string) => set((state) => ({ filename })),
+
+  isDeleteModalOpen: false,
+  setIsDeleteModalOpen: (open: boolean) => set((state) => ({ isDeleteModalOpen: open })),
+
+  isRenameModalOpen: false,
+  setIsRenameModalOpen: (open: boolean) => set((state) => ({ isRenameModalOpen: open })),
+}));
