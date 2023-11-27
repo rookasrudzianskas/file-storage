42d515ce45f6a1e2897e3dc2bd7c09e8553dfee6
Rokas <rookas.rudzenskas@gmail.com>
2023-11-27 14:31:05 +0200

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 import { UserButton } from "@clerk/nextjs";
+import {Button} from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="">
-
+      <Button>
+        Here is a demo button
+      </Button>
     </main>
   )
 }
