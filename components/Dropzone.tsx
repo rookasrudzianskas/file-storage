5935647bc7b6e8848be58157070cdbf29533bdb3
Rokas <rookas.rudzenskas@gmail.com>
2023-11-27 15:13:55 +0200

    dropbox

--- components/Dropzone.tsx
+++ components/Dropzone.tsx
@@ -2,17 +2,19 @@
 
 import React from 'react';
 import DropzoneComponent from 'react-dropzone'
+import {cn} from "@/lib/utils";
 
 const Dropzone = ({}) => {
   const maxSize = 20971520;
-
   return (
     <DropzoneComponent minSize={0} maxSize={maxSize} onDrop={acceptedFiles => console.log(acceptedFiles)}>
       {({getRootProps, getInputProps, isDragActive, isDragReject, fileRejections}) => {
         const isFileTooLarge = fileRejections.length > 0 && fileRejections[0].file.size > maxSize;
         return (
-          <section>
-            <div {...getRootProps()}>
+          <section className="m-4">
+            <div className={cn("w-full h-52  flex justify-center items-center p-5 border border-dashed rounded-lg" +
+              " text-center", isDragActive ? "bg-[#035FFE] text-white animate-pulse" : "bg-slate-100/50" +
+              " dark:bg-slate-800/50 text-slate-400")} {...getRootProps()}>
               <input {...getInputProps()} />
               {!isDragActive && 'Click here or drop a file to upload!'}
               {isDragActive && !isDragReject && "Drop it like it's hot!"}
